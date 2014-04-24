55b7c6c9f6227f5f4416c5e704693317204b8378
Geoffrey Litt <gklitt@gmail.com>
2014-04-24 07:30:33 -0400

    clean up arduino program

--- scan/scan.ino
+++ scan/scan.ino
@@ -1,9 +1,11 @@
 // Arduino code for laser scanning
 // Geoffrey Litt
 
+// This is a lightweight Arduino program which receives movement commands over
+// serial and outputs appropriate signals to two servos to move a pan-tilt.
 
-#include <Servo.h> 
- 
+#include <Servo.h>
+
 Servo ps; // phi servo
 Servo ts; // theta servo
 
@@ -11,24 +13,24 @@ const int PHI_PIN = 9;
 const int THETA_PIN = 10;
 
 void output(int p, int t){}
- 
+
 void setup(){
   ts.attach(10);
   ps.attach(9);
-  
+
   // initialize all the way down and turned clockwise
   ts.write(150);
   ps.write(180);
-  
+
   Serial.begin(9600);
-} 
+}
 
 void loop(){
   int phi; // phi angle
   int theta; // theta angle
   char inChar;
   String serialInput = "";
-  
+
   while(Serial.available() > 0){
     inChar = Serial.read();
     if(inChar == '\n'){
@@ -45,23 +47,3 @@ void loop(){
     }
   }
 }
-
-/*
-void loop() 
-{ 
-  for(pos = 180; pos>=1; pos -= 5)     // goes from 180 degrees to 0 degrees 
-  {                                
-    h.write(pos);              // tell servo to go to position in variable 'pos' 
-    //v.write(pos);
-    Serial.println(pos);
-    delay(1000);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 0; pos < 180; pos += 5)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    h.write(pos);              // tell servo to go to position in variable 'pos' 
-    //v.write(pos);
-    Serial.println(pos);
-    delay(1000);                       // waits 15ms for the servo to reach the position 
-  } 
-}
-*/
